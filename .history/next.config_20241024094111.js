d430e96f3076d9407117543a6945ead461838c5b
RunnerQuan <2776115684@qq.com>
2025-03-21 19:27:54 +0800

    chore: remove history folder from git tracking

--- .history/next.config_20241024094111.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-}
-
-module.exports = nextConfig
